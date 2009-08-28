057340bc9e507cb4d4da5c7233b85cc9a6b36949
Artefact2 <artefact2@gmail.com>
2009-08-29 00:08:41 +0200

    Use my new PluginL10n system for localization.

--- src/plugins/floghelper/FlogHelper.java
+++ src/plugins/floghelper/FlogHelper.java
@@ -4,25 +4,30 @@
 package plugins.floghelper;
 
 import freenet.clients.http.PageMaker.THEME;
-import freenet.l10n.L10n.LANGUAGE;
+import freenet.l10n.BaseL10n;
+import freenet.l10n.BaseL10n.LANGUAGE;
+import freenet.l10n.PluginL10n;
 import freenet.pluginmanager.FredPlugin;
 import freenet.pluginmanager.FredPluginHTTP;
 import freenet.pluginmanager.FredPluginThreadless;
 import freenet.pluginmanager.PluginHTTPException;
 import freenet.pluginmanager.PluginRespirator;
-import freenet.pluginmanager.FredPluginL10n;
+import freenet.pluginmanager.FredPluginBaseL10n;
 import freenet.pluginmanager.FredPluginThemed;
 import freenet.support.api.HTTPRequest;
-import plugins.floghelper.l10n.L10n;
 
 /**
  *
  * @author Artefact2
  */
-public class FlogHelper implements FredPlugin, FredPluginHTTP, FredPluginThreadless, FredPluginL10n, FredPluginThemed {
+public class FlogHelper implements FredPlugin, FredPluginHTTP, FredPluginThreadless, FredPluginBaseL10n, FredPluginThemed {
 	private PluginRespirator pr;
 	private THEME theme;
+	private static PluginL10n l10n;
 
+	public static BaseL10n getBaseL10n() {
+		return FlogHelper.l10n.getBase();
+	}
 
 	public void terminate() {
 		
@@ -30,26 +35,39 @@ public class FlogHelper implements FredPlugin, FredPluginHTTP, FredPluginThreadl
 
 	public void runPlugin(final PluginRespirator pr) {
 		this.pr = pr;
+		FlogHelper.l10n = new PluginL10n(this);
 	}
 
 	public String handleHTTPGet(final HTTPRequest request) throws PluginHTTPException {
-		return this.getString("HelloWorldTest");
+		return FlogHelper.getBaseL10n().getString("HelloWorldTest");
 	}
 
 	public String handleHTTPPost(final HTTPRequest request) throws PluginHTTPException {
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
-	public String getString(final String str) {
-		return L10n.getString(str);
-	}
-
-	public void setLanguage(final LANGUAGE lang) {
-		L10n.setLanguage(lang);
-	}
-
 	public void setTheme(final THEME theme) {
 		this.theme = theme;
+	}
+
+	public void setLanguage(BaseL10n.LANGUAGE arg0) {
+		FlogHelper.l10n = new PluginL10n(this, arg0);
+	}
+
+	public String getL10nFilesBasePath() {
+		return "plugins/floghelper/l10n/";
+	}
+
+	public String getL10nFilesMask() {
+		return "UI_${lang}.l10n";
+	}
+
+	public String getL10nOverrideFilesMask() {
+		return "UI_${lang}.override.l10n";
+	}
+
+	public ClassLoader getPluginClassLoader() {
+		return FlogHelper.class.getClassLoader();
 	}
 
 }
