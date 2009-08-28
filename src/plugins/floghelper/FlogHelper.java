913e6cd7f537d47e1cb078302c4f50e1aa5c53b7
Artefact2 <artefact2@gmail.com>
2009-08-28 18:40:23 +0200

    Add basic (broken atm) l10n.

--- src/plugins/floghelper/FlogHelper.java
+++ src/plugins/floghelper/FlogHelper.java
@@ -3,35 +3,53 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package plugins.floghelper;
 
+import freenet.clients.http.PageMaker.THEME;
+import freenet.l10n.L10n.LANGUAGE;
 import freenet.pluginmanager.FredPlugin;
 import freenet.pluginmanager.FredPluginHTTP;
 import freenet.pluginmanager.FredPluginThreadless;
 import freenet.pluginmanager.PluginHTTPException;
 import freenet.pluginmanager.PluginRespirator;
+import freenet.pluginmanager.FredPluginL10n;
+import freenet.pluginmanager.FredPluginThemed;
 import freenet.support.api.HTTPRequest;
+import plugins.floghelper.l10n.L10n;
 
 /**
  *
  * @author Artefact2
  */
-public class FlogHelper implements FredPlugin, FredPluginHTTP, FredPluginThreadless {
+public class FlogHelper implements FredPlugin, FredPluginHTTP, FredPluginThreadless, FredPluginL10n, FredPluginThemed {
 	private PluginRespirator pr;
+	private THEME theme;
 
 
 	public void terminate() {
 		
 	}
 
-	public void runPlugin(PluginRespirator pr) {
+	public void runPlugin(final PluginRespirator pr) {
 		this.pr = pr;
 	}
 
-	public String handleHTTPGet(HTTPRequest request) throws PluginHTTPException {
-		return "Hello world !";
+	public String handleHTTPGet(final HTTPRequest request) throws PluginHTTPException {
+		return this.getString("HelloWorldTest");
 	}
 
-	public String handleHTTPPost(HTTPRequest request) throws PluginHTTPException {
+	public String handleHTTPPost(final HTTPRequest request) throws PluginHTTPException {
 		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public String getString(final String str) {
+		return L10n.getString(str);
+	}
+
+	public void setLanguage(final LANGUAGE lang) {
+		L10n.setLanguage(lang);
+	}
+
+	public void setTheme(final THEME theme) {
+		this.theme = theme;
 	}
 
 }
