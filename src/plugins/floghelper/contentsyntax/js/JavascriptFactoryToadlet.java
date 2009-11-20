0531a1c82a1716db8288c71b53718ae4ac68c0d2
Artefact2 <artefact2@gmail.com>
2009-11-20 22:08:31 +0100

    Use proper GPLv3 headers.

--- src/plugins/floghelper/contentsyntax/js/JavascriptFactoryToadlet.java
+++ src/plugins/floghelper/contentsyntax/js/JavascriptFactoryToadlet.java
@@ -1,6 +1,19 @@
-/* This code is part of Freenet. It is distributed under the GNU General
- * Public License, version 2 (or at your option any later version). See
- * http://www.gnu.org/ for further details of the GPL. */
+/* FlogHelper, Freenet plugin to create flogs
+ * Copyright (C) 2009 Romain "Artefact2" Dalmaso
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package plugins.floghelper.contentsyntax.js;
 
 import freenet.client.HighLevelSimpleClient;
