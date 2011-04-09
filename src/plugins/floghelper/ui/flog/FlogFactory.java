c32cfde56944c005ff1d041349fc86c203cb4559
Artefact2 <artefact2@gmail.com>
2011-04-09 17:17:26 +0200

    Use a more human-readable insert identifier. Include flog name.

--- src/plugins/floghelper/ui/flog/FlogFactory.java
+++ src/plugins/floghelper/ui/flog/FlogFactory.java
@@ -556,7 +556,7 @@ public class FlogFactory {
 					 * FCPServer server,
 					 * ObjectContainer container
 					 */
-					ClientPutDir cpd = new ClientPutDir(client, uri, "FlogHelper-" + flog.getID() + "-" + DataFormatter.getRandomID(14), Integer.MAX_VALUE, RequestStarter.IMMEDIATE_SPLITFILE_PRIORITY_CLASS, ClientRequest.PERSIST_FOREVER, null, false, false, -1, parsedFlog, "index.html", true, false, false, true, 2, 2, false, fcp, arg0);
+					ClientPutDir cpd = new ClientPutDir(client, uri, "FlogHelper: " + flog.getTitle() + " (" + flog.getID() + DataFormatter.getRandomID(4) + ")", Integer.MAX_VALUE, RequestStarter.IMMEDIATE_SPLITFILE_PRIORITY_CLASS, ClientRequest.PERSIST_FOREVER, null, false, false, -1, parsedFlog, "index.html", true, false, false, true, 2, 2, false, fcp, arg0);
 					try {
 						fcp.startBlocking(cpd, arg0, arg1);
 						WoTContexts.addContext(flog.getAuthorID());
