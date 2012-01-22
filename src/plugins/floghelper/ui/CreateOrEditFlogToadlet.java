7c524af235dc8e043f26a41e9c972f874185165f
Steve Dougherty <steve@asksteved.com>
2012-01-22 10:05:07 -0500

    Specify theme even in simple mode.

--- src/plugins/floghelper/ui/CreateOrEditFlogToadlet.java
+++ src/plugins/floghelper/ui/CreateOrEditFlogToadlet.java
@@ -247,6 +247,16 @@ public class CreateOrEditFlogToadlet extends FlogHelperToadlet {
 					.addChild("input", new String[]{"type", "size", "name", "value", "maxlength"},
 					new String[]{"text", "20", "SSKPath", flog.getSSKPath(), Integer.toString(SSKPATH_MAXLENGTH)});
 
+			settingsBox.addChild("p").addChild("label", "for", "Theme", FlogHelper.getBaseL10n().getString("ThemeFieldDesc")).addChild("br");
+
+			final HTMLNode themesBox = settingsBox.addChild("select", new String[]{"id", "name"}, new String[]{"Theme", "Theme"});
+			for (final String theme : FlogFactory.THEMES) {
+				final HTMLNode option = themesBox.addChild("option", "value", theme, theme);
+				if (flog.getTheme().equals(theme)) {
+					option.addAttribute("selected", "selected");
+				}
+			}
+
 			if(ctx.getContainer().isAdvancedModeEnabled()) {
 			final HTMLNode templatesBox = this.getPM().getInfobox(null, FlogHelper.getBaseL10n().getString("Templates"), form, "TemplatesFlogData", true);
 
@@ -261,16 +271,6 @@ public class CreateOrEditFlogToadlet extends FlogHelperToadlet {
 			checkBlock.addChild("br");
 			checkBlock.addChild("a", "href", FlogHelperToadlet.BASE_URI + PreviewToadlet.MY_URI + flog.getID() + "/" + PreviewToadlet.VIEW_RAW_DEFAULT_TEMPLATE_URI,
 					FlogHelper.getBaseL10n().getString("SeeTheRawDefaultTemplate"));
-
-			final HTMLNode themesBox = new HTMLNode("select", new String[]{"id", "name"}, new String[]{"Theme", "Theme"});
-			for (final String theme : FlogFactory.THEMES) {
-				final HTMLNode option = themesBox.addChild("option", "value", theme, theme);
-				if (flog.getTheme().equals(theme)) {
-					option.addAttribute("selected", "selected");
-				}
-			}
-
-			templatesBox.addChild("p").addChild("label", "for", "Theme", FlogHelper.getBaseL10n().getString("ThemeFieldDesc")).addChild("br").addChild(themesBox);
 
 			final boolean overrideCSS = flog.overrideCSS();
 			checkBlock = templatesBox.addChild("p");
